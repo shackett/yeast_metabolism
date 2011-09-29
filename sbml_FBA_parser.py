123750ba3abd29b057c4e33d19f5189a6982a54b
shackett <shackett@princeton.edu>
2011-09-29 16:24:19 -0400

    9.29.11-2

--- sbml_FBA_parser.py
+++ sbml_FBA_parser.py
@@ -55,49 +55,63 @@ def write_resource_file(model, outfile):
 
 
 def IDdict(model):
-	""" Create a corresponence between species type designation and ID """
+	""" Create a correspondence between species type designation and ID """
 	name_corr = dict()
 	listSpecies = model.getListOfSpecies()
 	for lst in listSpecies:
 		name_corr[lst.getId()] = lst.getSpeciesType()
 	return name_corr
 
-def write_rxn(model, outfile, modifiers = "TRUE"):
+def write_rxn(model, outfile, rxn_par_found = "TRUE", rxn_kineticForm = "TRUE"
+, use_modifiers = "TRUE"):
 
+	"""writes a rxn-file describing the stoichiometry and (optionally) modifiers of a
+	chemical reaction.  writes a spec-file describing the association between a metabolite/
+	protein internal name in a rxn/compartment and a single chemical-specific identifier; 
+	also notes the compartment.  optionally writes reaction parameters and reaction kinetics."""
+	
 	listrxn = model.getListOfReactions()
 	listspecT = model.getListOfSpeciesTypes()
 	listspec = model.getListOfSpecies()
-
+	
 	rxn_outfile = "".join(("rxn_", outfile, ".tsv"))
-	#rxn_par_outfile = "".join(("rxn_par_", outfile, ".tsv"))
-	spec_outfile = "".join(("spec_", outfile, ".tsv"))
-
 	rxn_outf = open(rxn_outfile, 'w')
-	rxn_outf.write("%s\t%s\t%s\t%s\n" % ("Reaction", "ReactionID", "Metabolite", "StoiCoef"))
+	rxn_outf.write("%s\t%s\t%s\t%s\n" % ("Reaction", "ReactionID", "Metabolite", 
+	"StoiCoef"))
 	rxn_outf.close()	
-
-	#par_outf = open(rxn_par_outfile, 'w')
-	#par_outf.write("%s\t%s\t%s\t%s\n" % ("ReactionID", "ParID", "Value", "Units"))
-	#par_outf.close()
-
+	
+	spec_outfile = "".join(("spec_", outfile, ".tsv"))
 	spec_outf = open(spec_outfile, 'w')
-	spec_outf.write("%s\t%s\t%s\t%s\n" % ("SpeciesID", "SpeciesName", "SpeciesType", "Compartment"))
+	spec_outf.write("%s\t%s\t%s\t%s\n" % ("SpeciesID", "SpeciesName", "SpeciesType"
+	, "Compartment"))
 	spec_outf.close()
-
-
+	
+	if rxn_par_found == "TRUE":
+		rxn_par_outfile = "".join(("rxn_par_", outfile, ".tsv"))
+		par_outf = open(rxn_par_outfile, 'w')
+		par_outf.write("%s\t%s\t%s\t%s\n" % ("ReactionID", "ParID", "Value", "Units"))
+		par_outf.close()
+	
+		rxn_kinetic_outfile = "".join(("rxn_kinetic_", outfile, ".tsv"))
+		kin_outf = open(rxn_kinetic_outfile, 'w')
+		kin_outf.write("%s\t%s\t%s\n" % ("ReactionID", "ParID", "Formula"))
+		kin_outf.close()
+	
+	
 	met_namedict = dict()
 	for spect in listspecT:
 		met_namedict[spect.getId()] = spect.getName()
-
+	
 	spec_outf = open(spec_outfile, 'a')	
 	for spec in listspec:
-		spec_outf.write("%s\t%s\t%s\t%s\n" % (spec.getId(), met_namedict[spec.getSpeciesType()], spec.getSpeciesType(), spec.getCompartment()))
+		spec_outf.write("%s\t%s\t%s\t%s\n" % (spec.getId(), 
+		met_namedict[spec.getSpeciesType()], spec.getSpeciesType(), spec.getCompartment()))
 	spec_outf.close()	
-
+	
 	for rxn in listrxn:
-	
+		
 	### Get the reactants and products and their stoichiometries, along with any rxn modifiers
-	
+		
 		rxn_outf = open(rxn_outfile, 'a')
 		for lst in rxn.getListOfReactants():
 			rxn_outf.write("%s\t%s\t%s\t%s\n" % (rxn.getName(), rxn.getId(), lst.getSpecies(), 
@@ -105,9 +119,27 @@ def write_rxn(model, outfile, modifiers = "TRUE"):
 		for lst in rxn.getListOfProducts():
 			rxn_outf.write("%s\t%s\t%s\t%s\n" % (rxn.getName(), rxn.getId(), lst.getSpecies(), 
 			lst.getStoichiometry()))
-		for lst in rxn.getListOfModifiers():
-			rxn_outf.write("%s\t%s\t%s\n" % (rxn.getName(), rxn.getId(), lst.getSpecies()))
-		rxn_outf.close()
+		
+		if use_modifiers == "TRUE":
+			for lst in rxn.getListOfModifiers():
+				rxn_outf.write("%s\t%s\t%s\n" % (rxn.getName(), rxn.getId(), lst.getSpecies()))
+			rxn_outf.close()	
+		
+		if rxn_par_found == "TRUE":
+		
+			parameters = list(rxn.getKineticLaw().getListOfParameters())
+			if len(parameters) > 0:
+				par_outf = open(rxn_par_outfile, 'w')
+				for p in parameters:
+					par_outf.write("%s\t%s\t%s\t%s\n" % (rxn.getId(), p.getId(), 
+					p.getValue(), p.getUnits()))
+				par_outf.close()
+				
+			kin_outf = open(rxn_kinetic_outfile, 'a')
+			kin_outf.write("%s\t%s\n" % (rxn.getId(), rxn.getKineticLaw().getFormula()))
+			kin_outf.close()
+			
+
 		
 ### MAIN ###
 
@@ -130,13 +162,12 @@ model = doc.getModel()
 species_par_outfile = "".join(("species_par_", outfile, ".tsv"))
 write_resource_file(model, species_par_outfile)
 
-write_rxn(model, outfile, modifiers = "TRUE")
+write_rxn(model, outfile, rxn_par_found = "TRUE", rxn_kineticForm = "TRUE"
+, use_modifiers = "TRUE")
 
 	
 	
-	
-	
-	
+			
 
 	
 		
