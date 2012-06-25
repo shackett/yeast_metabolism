689b78c51eed07bfce3b30ecdb2acb3b8370d17c
shackett <shackett@princeton.edu>
2012-06-25 14:16:15 -0400

    matching yeast to ecoli rxns via EC numbers

--- gibbsAnalysis.R
+++ gibbsAnalysis.R
@@ -1,7 +1,327 @@
+options(stringsAsFactors = FALSE)
+
+setwd("~/Desktop/Rabinowitz/FBA_SRH/")
+source("Yeast_genome_scale/FBA_lib.R")
+
+
+inputFilebase = "yeast"
+run_full = FALSE
+
+rxnFile = read.delim(paste("Yeast_genome_scale/rxn_", inputFilebase, ".tsv", sep = ""))
+rxnparFile = read.delim(paste("Yeast_genome_scale/species_par_", inputFilebase, ".tsv", sep = ""), header = FALSE)
+corrFile = read.delim(paste("Yeast_genome_scale/spec_", inputFilebase, ".tsv", sep = ""))
+compFile <- read.delim(paste("Yeast_genome_scale/comp_", inputFilebase, ".tsv", sep = ""))
+
+reactions = unique(rxnFile$ReactionID)
+rxnStoi <- rxnFile[is.na(rxnFile$StoiCoef) == FALSE,]
+
+if(file.exists("Yeast_genome_scale/yeast_stoi.R")){
+	load("Yeast_genome_scale/yeast_stoi.R")
+	} else {write_stoiMat(metabolites, reactions, corrFile, rxnFile, internal_names = TRUE)}
+
+
+
+
+
+
+
+metabolites <- rownames(stoiMat)
+
+e_coli_mets <- read.delim("EcoliYeastMatch/gibbs_mets.txt", header = TRUE, sep = "\t")
+e_coli_mets[,1:3] <- apply(e_coli_mets[,1:3], c(1,2), function(x){
+	if(x == "''"){NA}else{
+	strsplit(x, split = "'")[[1]][2]
+	}})
+e_coli_rxns <- read.delim("EcoliYeastMatch/gibbs_rxns.txt", header = TRUE, sep = "\t")
+e_coli_rxns[,c(1,3)] <- apply(e_coli_rxns[,c(1,3)], c(1,2), function(x){
+	if(x == "''"){NA}else{
+	strsplit(x, split = "'")[[1]][2]
+	}})
+
+
+e_coli_S <- as.matrix(read.delim("EcoliYeastMatch/e_coliStoi", header = FALSE, sep = ",", stringsAsFactors = FALSE))
+rownames(e_coli_S) <- e_coli_mets$ID
+colnames(e_coli_S) <- e_coli_rxns$ID
+
+
+if(run_full == TRUE){
+	#write out the ecoli KEGG IDs 
+	
+	write.table(e_coli_mets[,c(1,3)], file = "ecoli_kegg_id.csv", sep = ",", col.names = FALSE, row.names = FALSE, quote = FALSE)
+
+	#write out the yeast ChEBI IDs
+	
+	add.chebi.comp <- read.delim("Yeast_genome_scale/chebi_srh_curated.tsv")
+	add.chebi.comp <- add.chebi.comp[is.na(add.chebi.comp$generic),]
+	add.chebi.comp$internal_ID <- sapply(add.chebi.comp$internal_ID, function(x){corrFile$SpeciesType[corrFile$SpeciesID == x]})
+
+	yeastChEBI <- cbind(metabolites, as.numeric(unlist(sapply(metabolites, metToCHEBI))))
+
+	for(el in 1:length(add.chebi.comp[,1])){
+		new_ids <- corrFile$SpeciesID[corrFile$SpeciesType == add.chebi.comp$internal_ID[el]]
+		if(length(new_ids) == 1){
+			yeastChEBI[c(1:length(yeastChEBI[,1]))[yeastChEBI[,1] == new_ids],2] <- add.chebi.comp$CHEBI_ID[el]
+			}else{
+				yeastChEBI[sapply(new_ids, function(x){c(1:length(yeastChEBI[,1]))[yeastChEBI[,1] == x]}),][,2] <- add.chebi.comp$CHEBI_ID[el]
+				}
+		}
+	
+	write.table(yeastChEBI, file = "yeast_chebi_id.csv", sep = ",", col.names = FALSE, row.names = FALSE, quote = FALSE)
+	
+	#use the script ecoliYeastMatch.py to match the yeast species ids to ecoli species ids
+	}	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+#match based on shared stoichiometry
+
+YE_match <- read.delim("EcoliYeastMatch/correspondence.dict.csv", sep = "\t", header = TRUE)
+YE_match <- YE_match[sapply(c(1:length(YE_match[,1])), function(x){c(1:length(YE_match[,1]))[YE_match[,1] == rownames(stoiMat)[x]]}),]
+
+YE_match_list <- list()
+for(spec in c(1:length(YE_match[,1]))){
+	YE_match_list[[YE_match[spec,1]]] <- unlist(strsplit(YE_match[spec,2], split = ","))
+	
+	}
+
+#for each yeast rxn: read the rows of the ecoli stoichiometry matrix corresponding to the species matched with yeast.  Look for matching stoichiometry
+
+perfect_matches <- list()
+near_matches <- list()
+is_perfect <- rep(NA, times = length(stoiMat[1,]))
+
+for(yRx in 1:length(stoiMat[1,])){
+	
+	rxn_stoi <- stoiMat[stoiMat[,yRx] != 0, yRx]
+	if(length(rxn_stoi) == 1){
+		names(rxn_stoi) <- rownames(stoiMat)[stoiMat[,yRx] != 0]
+		}
+			
+	YE_match_list[names(rxn_stoi)]
+	
+	spec_col_id <- NULL
+	matching_mat <- NULL
+	for(spec in 1:length(rxn_stoi)){
+		if(length(YE_match_list[names(rxn_stoi)][[spec]]) == 0){
+			print(paste("no match for", names(rxn_stoi)[spec], ":", metIDtoSpec(names(rxn_stoi)[spec])))
+			}else{
+				matching_mat <- rbind(matching_mat, matrix(e_coli_S[rownames(e_coli_S) %in% YE_match_list[names(rxn_stoi)][[spec]],], nrow = length(YE_match_list[names(rxn_stoi)][[spec]])))
+				spec_col_id <- c(spec_col_id, rep(spec, times = length(YE_match_list[names(rxn_stoi)][[spec]])))
+			}
+		}
+	
+	if(is.null(spec_col_id)){
+		next
+		}
+	
+	unity_mat <- matching_mat * 1/matrix(sapply(spec_col_id, function(x){rxn_stoi[x]}), nrow = length(spec_col_id), ncol = length(e_coli_S[1,]))
+	
+	pos_match <- NULL
+	neg_match <- NULL
+	
+	for(spec in 1:length(rxn_stoi)){
+		if(length(spec_col_id[spec_col_id == spec]) == 0){
+			pos_match <- rbind(pos_match, rep(FALSE, times = length(e_coli_S[1,])))
+			neg_match <- rbind(neg_match, rep(FALSE, times = length(e_coli_S[1,])))
+			}else{
+				if(length(spec_col_id[spec_col_id == spec]) == 1){
+					pos_match <- rbind(pos_match, unity_mat[c(1:length(spec_col_id))[spec_col_id == spec],] == 1)
+					neg_match <- rbind(neg_match, unity_mat[c(1:length(spec_col_id))[spec_col_id == spec],] == -1)
+					}else{
+						pos_match <- rbind(pos_match, apply(unity_mat[c(1:length(spec_col_id))[spec_col_id == spec],] == 1, 2, sum) != 0)
+						neg_match <- rbind(neg_match, apply(unity_mat[c(1:length(spec_col_id))[spec_col_id == spec],] == -1, 2, sum) != 0)
+						}
+					}
+			}
+	
+	near_matches[[colnames(stoiMat)[yRx]]] <- list()
+	perfect_matches[[colnames(stoiMat)[yRx]]] <- list()
+							
+	
+	if(length(rxn_stoi) > 1){
+		pos_match <- apply(pos_match, 2, sum)
+		neg_match <- apply(neg_match, 2, sum)
+		
+		most_similar <- max(c(pos_match, neg_match))
+		
+		if(most_similar == length(rxn_stoi)){
+			is_perfect[yRx] <- TRUE
+			if(length(c(1:length(pos_match))[pos_match == most_similar]) != 0){
+				perfect_matches[[c(1:length(names(perfect_matches)))[names(perfect_matches) == colnames(stoiMat)[yRx]]]]$"F" <- c(1:length(pos_match))[pos_match == most_similar]
+				}else{
+					perfect_matches[[c(1:length(names(perfect_matches)))[names(perfect_matches) == colnames(stoiMat)[yRx]]]]$"F" <- NA
+					}
+			if(length(c(1:length(neg_match))[neg_match == most_similar]) != 0){
+				perfect_matches[[c(1:length(names(perfect_matches)))[names(perfect_matches) == colnames(stoiMat)[yRx]]]]$"R" <- c(1:length(neg_match))[neg_match == most_similar]
+				}else{
+					perfect_matches[[c(1:length(names(perfect_matches)))[names(perfect_matches) == colnames(stoiMat)[yRx]]]]$"R" <- NA
+					}
+			
+			}else{
+				is_perfect[yRx] <- FALSE
+				}
+		
+		if(most_similar >= length(rxn_stoi)/2){
+			
+			n_matches <- table(c(pos_match, neg_match))
+			most_similar <- max(names(n_matches)[names(n_matches) != length(rxn_stoi)])
+			
+			if(length(c(1:length(pos_match))[pos_match == most_similar]) != 0){
+				near_matches[[c(1:length(names(near_matches)))[names(near_matches) == colnames(stoiMat)[yRx]]]]$"F" <- c(1:length(pos_match))[pos_match == most_similar]
+				}else{
+					near_matches[[c(1:length(names(near_matches)))[names(near_matches) == colnames(stoiMat)[yRx]]]]$"F" <- NA
+					}
+			if(length(c(1:length(neg_match))[neg_match == most_similar]) != 0){
+				near_matches[[c(1:length(names(near_matches)))[names(near_matches) == colnames(stoiMat)[yRx]]]]$"R" <- c(1:length(neg_match))[neg_match == most_similar]
+				}else{
+					near_matches[[c(1:length(names(near_matches)))[names(near_matches) == colnames(stoiMat)[yRx]]]]$"R" <- NA
+					}
+			
+			}else{
+				near_matches[[c(1:length(names(near_matches)))[names(near_matches) == colnames(stoiMat)[yRx]]]]$"F" <- NA
+				near_matches[[c(1:length(names(near_matches)))[names(near_matches) == colnames(stoiMat)[yRx]]]]$"R" <- NA
+				}
+		}
+	}
+		
+
+
+#breakdown of assignment
+c(table(is_perfect), nNA = table(is.na(is_perfect))[names(table(is.na(is_perfect))) == "TRUE"])
+
+#determine whether perfect matches are unique		
+perfect <- c(1:length(is_perfect))[!is.na(is_perfect)][is_perfect[!is.na(is_perfect)]]
+n_matches <- rep(NA, times = length(perfect))
+rxn_free_e <- data.frame(delta_G_Kj_mol = rep(NA, times = length(perfect)), G_form_sd = rep(NA, times = length(perfect)))
+for(match in perfect){
+	n_matches[c(1:length(perfect))[perfect == match]] <- length(unlist(perfect_matches[[colnames(stoiMat)[match]]]))
+	
+	rxn_free_e <- 
+	
+	
+	perfect_matches[[colnames(stoiMat)[match]]]
+	
+	e_coli_rxns[2047,]
+	
+	}
+
+imperfect <- perfect[n_matches != 1]
+
+
+
+
+
+	
+		
+		
+	
+	
+		
+	
+	
+	
+	
+	
+
+#match based on shared E.C. numbers
+
+e_coli_genes <- read.table("EcoliYeastMatch/gibbs_genes.txt", sep = "\t")
+e_coli_rxn_corr <- read.table("EcoliYeastMatch/rxnGeneMat", sep = ",")
+#ecoli_dict <- read.table("EcoliYeastMatch/ecoliNameDict.txt", sep = "\t", header = TRUE)
+ecoli_dict <- read.table("EcoliYeastMatch/gene-links.dat", skip = 16, sep = "\t")
+
+e_coli_gene_names <- unlist(sapply(e_coli_genes[,1], function(ID){ifelse(ID %in% ecoli_dict[,3], ecoli_dict[,7][ecoli_dict[,3] == ID], NA)}))
+
 #source("http://bioconductor.org/biocLite.R")
-#biocLite("org.Sc.sgd.db")
-source("http://bioconductor.org/biocLite.R")
-biocLite("org.EcK12.eg.db")
+#biocLite("org.EcK12.eg.db")
+
+library("org.EcK12.eg.db")
+# Convert the object to a list
+name_to_entrez_list <- as.list(org.EcK12.egALIAS2EG)
+
+name_to_entrez <- sapply(e_coli_gene_names, function(ID){
+	if(is.na(ID)){
+		NA
+		}else{
+		if(ID %in% names(name_to_entrez_list)){
+			name_to_entrez_list[ID]
+			}else{
+				NA
+				}
+		}
+	})
+
+entrez_to_EC_list <- as.list(org.EcK12.egENZYME)
+
+entrez_to_EC <- sapply(name_to_entrez, function(ID){
+	if(is.na(ID[1])){
+		NA
+		}else{
+			ECnums <- NULL
+			for(i in 1:length(ID)){
+				if(ID[i] %in% names(entrez_to_EC_list)){
+					ECnums <- union(ECnums, entrez_to_EC_list[names(entrez_to_EC_list) == ID[i]])
+					}
+				}
+			if(length(ECnums) == 0){
+				NA}else{
+					ECnums
+					}
+			}
+	})
+
+### Pass free energy to E.C. numbers
+
+e_coli_rxns
+
+unique_ECs <- unique(unlist(entrez_to_EC)[!is.na(unlist(entrez_to_EC))])
+e_coli_EC_gibbs <- list()
+for(ID in unique_ECs){
+	e_coli_EC_gibbs[ID] <- NA
+	}
+
+
+for(gene in 1:length(e_coli_rxn_corr[1,])){
+	
+	ECvals <- unlist(entrez_to_EC[[gene]])
+	if(!is.na(ECvals[1])){
+		for(ECval in ECvals){
+			if(is.vector(e_coli_EC_gibbs[[ECval]])){
+				e_coli_EC_gibbs[[ECval]] <- e_coli_rxns[e_coli_rxn_corr[,gene] == 1,]
+				}else{
+					e_coli_EC_gibbs[[ECval]] <- rbind(e_coli_EC_gibbs[[ECval]], e_coli_rxns[e_coli_rxn_corr[,gene] == 1,])
+					}
+			}
+		}
+	}
+
+#determine the EC numbers of yeast genes
+
+
+
+
+rxnFile
+
+
+
+
+
+
+
+
+
+
+
 
 #all of the mapped genes in yeast
 mapped_genes
@@ -15,22 +335,6 @@ x <- org.Sc.sgdENZYME
   
     }    
 
-library("org.EcK12.eg.db")
-
-library("org.EcK12.eg.db")
-y <- org.EcK12.egENZYME
-	# Get the ORF identifiers that are mapped to an EC number 
-    mapped_genes <- mappedkeys(y)
-    # Convert to a list
-    yy <- as.list(y[mapped_genes])
-    if(length(yy) > 0) {
-      # Get the ENZYME id for the first five genes
-      yy[1:5]
-      # Get the first one
-      yy[[1]]
-    }    
-
-table(unique(unlist(yy)) %in% unique(unlist(xx)))
 table(unique(unlist(xx)) %in% unique(unlist(yy)))
 
 
@@ -38,145 +342,9 @@ table(unique(unlist(xx)) %in% unique(unlist(yy)))
 
 org.Sc.sgdENZYME()
 
-setwd("~/Desktop/Rabinowitz/FBA_SRH/")
-source("Yeast_genome_scale/FBA_lib.R")
-
-
-options(stringsAsFactors = FALSE)
-
-inputFilebase = "yeast"
-
-rxnFile = read.delim(paste("Yeast_genome_scale/rxn_", inputFilebase, ".tsv", sep = ""), stringsAsFactors = FALSE)
-rxnparFile = read.delim(paste("Yeast_genome_scale/species_par_", inputFilebase, ".tsv", sep = ""), header = FALSE, stringsAsFactors = FALSE)
-corrFile = read.delim(paste("Yeast_genome_scale/spec_", inputFilebase, ".tsv", sep = ""), stringsAsFactors = FALSE)
-compFile <- read.delim(paste("Yeast_genome_scale/comp_", inputFilebase, ".tsv", sep = ""), stringsAsFactors = FALSE)
-
-reactions = unique(rxnFile$ReactionID)
-rxnStoi <- rxnFile[is.na(rxnFile$StoiCoef) == FALSE,]
-
-if(file.exists("Yeast_genome_scale/yeast_stoi.R")){
-	load("Yeast_genome_scale/yeast_stoi.R")
-	} else {write_stoiMat(metabolites, reactions, corrFile, rxnFile, internal_names = TRUE)}
-
-metabolites <- rownames(stoiMat)
-#write out the yeast ChEBI IDs
-yeastChEBI <- cbind(metabolites, as.numeric(unlist(sapply(metabolites, metToCHEBI))))
-write.table(yeastChEBI, file = "yeast_chebi_id.csv", sep = ",", col.names = FALSE, row.names = FALSE, quote = FALSE)
-
-#write out the ecoli KEGG IDs 
-e_coli_mets <- read.delim("gibbs_mets.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-e_coli_rxns <- read.delim("gibbs_rxns.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-e_coli_S <- as.matrix(read.delim("e_coliStoi", header = FALSE, sep = ",", stringsAsFactors = FALSE))
-
-e_coli_mets[,1:3] <- apply(e_coli_mets[,1:3], c(1,2), function(x){
-	if(x == "''"){NA}else{
-	strsplit(x, split = "'")[[1]][2]
-	}})
-
-write.table(e_coli_mets[,c(1,3)], file = "ecoli_kegg_id.csv", sep = ",", col.names = FALSE, row.names = FALSE, quote = FALSE)
 
 
 
 
 
 
-
-
-
-
-
-
-
-#read in to get a list of yeast chebi:KEGG & chemical species synonyms
-yeastCTS_1 <- read.table("yeast_CTS_output.csv", sep = ";", header = TRUE)
-
-ChEBIsyn <- NULL
-for(i in 1:length(yeastCTS_1[,1])){
-	if(!(yeastCTS_1$Synonyms[i] %in% c("None", ""))){ 
-		i_syn <- unlist(strsplit(yeastCTS_1$Synonyms[i], ", "))
-		i_syn <- sub('[^:space:+]', "", i_syn)
-		i_syn <- unname(sapply(i_syn, function(x){unlist(strsplit(x, " - "))[1]}))
-		ChEBIsyn <- rbind(ChEBIsyn, data.frame(yChEBI = yeastCTS_1$From[i], ySyn = i_syn))
-		}
-	}
-	
-ChEBIsyn <- ChEBIsyn[ChEBIsyn[,2] != "",]
-	
-	
-joint.ids <- sapply(c(1:length(ChEBIsyn[,1])), function(x){
-	paste(ChEBIsyn[x,1], ChEBIsyn[x,2], sep = "fubar")
-	})
-
-joint.mat <- matrix(unlist(sapply(unique(joint.ids), function(x){strsplit(x, split = "fubar")})), ncol = 2, byrow = TRUE)
-	
-		
-write.table(joint.mat[1:6000,2], file = "yeast_syn1.tsv", col.names = FALSE, row.names = FALSE, quote = FALSE)
-write.table(joint.mat[6001:length(joint.mat[,1]), 2], file = "yeast_syn2.tsv", col.names = FALSE, row.names = FALSE, quote = FALSE)
-
-
-
-#get a kegg id for each metabolite
-
-
-
-keggChebiCorr <- read.table("Yeast_reconstruction/Sequences/database_accession.tsv", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
-keggChebiCorr <- keggChebiCorr[grep("KEGG", keggChebiCorr$TYPE),]
-
-met_dict <- data.frame("yeastID" = metabolites, "ChEBI" = as.numeric(unlist(sapply(metabolites, metToCHEBI))), "KEGG" = NA)
-
-matched_mets <- met_dict$ChEBI[c(1:length(met_dict[,1]))[(met_dict$ChEBI %in% keggChebiCorr$COMPOUND_ID)]]
-#keggChebiCorr[keggChebiCorr$COMPOUND_ID %in% matched_mets,]
-
-met_dict$KEGG <- sapply(met_dict$ChEBI, function(x){
-	if(x %in% keggChebiCorr$COMPOUND_ID){
-		keggChebiCorr$ACCESSION_NUMBER[keggChebiCorr$COMPOUND_ID %in% x]
-		}else{
-			NA
-			}})
-		
-	
-	 
-
-#cross-reference unique chemical names with ChEBI and KEGG using CTS to create a degenerate correspondence between these species identifiers
-CTS_degen_files <- list.files(pattern = "yCTS")
-unlist(strsplit(CTS_degen_files, split = "yCTS_"))
-files_numz <- as.numeric(sapply(CTS_degen_files, function(x){unlist(strsplit(unlist(strsplit(x, split = "yCTS_"))[2], split = ".csv"))[1]}))
-
-import_order <- CTS_degen_files[order(files_numz)]
-degen_library <- NULL
-
-for(a_file in import_order){
-	extending_lib <- read.delim(a_file, sep = ";", header = TRUE, stringsAsFactors = FALSE)
-	extending_lib <- extending_lib[,colnames(extending_lib) %in% c("From", "Name", "CAS", "CHEBI", "KEGG")]
-	
-	
-	}
-
-
-
-
-
-
-#crossreference the Ecoli IDs with ChEBI and CAS using CTS
-
-
-
-
-
-
-e_coli_mets <- 
-
-unique_rxns <- 
-
-#reducing the size of the stoichiometric matrix by consolidating reactions with identical stoichiometry
-
-n.rx = length(e_coli_rxns[,1]); n.met = length(e_coli_mets[,1])
-
-
-
-
-#u.mat <- matrix(NA, ncol = n.rx, nrow = n.rx)
-#for(i in 1:n.rx){
-#	tmp <- matrix(e_coli_S[,i], ncol = n.rx, n.met) - e_coli_S
-#	u.mat[i,] <- apply(tmp != 0, 2, sum) == 0
-#	}
