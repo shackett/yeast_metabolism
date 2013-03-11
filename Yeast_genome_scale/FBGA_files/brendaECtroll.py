e0381fa45cff07a585cf9e1f5ed68d65197949ae
shackett <shackett@princeton.edu>
2013-03-11 14:23:51 -0400

    searching through all organisms kinetic values

--- Yeast_genome_scale/FBGA_files/brendaECtroll.py
+++ Yeast_genome_scale/FBGA_files/brendaECtroll.py
@@ -7,23 +7,36 @@ import re
 from SOAPpy import WSDL ## for extracting the URL of the endpoint (server script) from the WSDL file
 from SOAPpy import SOAPProxy ## for usage without WSDL file
 
+def removeNonAscii(s): return "".join(filter(lambda x: ord(x)<128, s)) #remove non Ascii charcters - the angstrom symbol
 
-ec_file = 'ECnr.txt'
+ec_file = 'ECnr.txt' #a text file with E.C. numbers to be queried against BRENDA
+#ec_file = 'ECnr_red.txt'
 organism = 'Saccharomyces cerevisiae'
+use_organism = 'TRUE'
 
+if use_organism == 'TRUE':
+	outfile = "_".join((organism.split(' ')[0].strip()[0], organism.split(' ')[1]))
+else:
+	outfile = "all"
+ 
 endpointURL = "http://www.brenda-enzymes.info/soap2/brenda_server.php"
 client = SOAPProxy(endpointURL)
+
+
 
 
 observedLigands = set()
 
 """ Determine the Km values for all metabolites associated with an E.C. number """
 
-km_out = open("km_file.tsv", "w")
+km_out = open("".join((outfile, "_km.tsv")), "w")
 km_out.write("ecNumber\tkmValue\tkmValueMaximum\tsubstrate\tcommentary\torganism\tligandStructureId\tliterature" + "\n")
 
 for l in open(ec_file):
-	resultString = client.getKmValue("".join(("ecNumber*", re.split('\n',  l)[0], "#organism*", organism))).split('!')
+	if use_organism == 'TRUE':
+		resultString = client.getKmValue("".join(("ecNumber*", re.split('\n',  l)[0], "#organism*", organism))).split('!')
+	else:
+		resultString = client.getKmValue("".join(("ecNumber*", re.split('\n',  l)[0]))).split('!')
 	 
 	for entry in resultString: #split each Brenda Km entry into a separate line
 		
@@ -31,12 +44,14 @@ for l in open(ec_file):
 		all_fields = entry.split('#')
 		all_fields.remove('')
 		
-		if len(all_fields) == 8:
-			observedLigands.add(all_fields[7].split('*')[1]) #add ligandID to set of ligand IDs
+		if len(all_fields) != 8:
+			continue
+		observedLigands.add(all_fields[7].split('*')[1]) #add ligandID to set of ligand IDs
 		
 		entry_write = []
 		for field in all_fields:
 			entry_write.append(field.split('*')[1])
+		entry_write[4] = removeNonAscii(entry_write[4]) #some description include non ascii characters
 		
 		km_out.write("\t".join(entry_write) + "\n")
 	
@@ -45,23 +60,30 @@ km_out.close()
 
 """ Determine the Ki values for all metabolites associated with an E.C. number """
 
-ki_out = open("ki_file.tsv", "w")
+ki_out = open("".join((outfile, "_ki.tsv")), "w")
 ki_out.write("ecNumber\tkiValue\tkiValueMaximum\tinhibitor\tcommentary\torganism\tligandStructureId\tliterature" + "\n")
 
 for l in open(ec_file):
-	resultString = client.getKiValue("".join(("ecNumber*", re.split('\n',  l)[0], "#organism*", organism))).split('!')
-	 
+	if use_organism == 'TRUE':
+		resultString = client.getKiValue("".join(("ecNumber*", re.split('\n',  l)[0], "#organism*", organism))).split('!')
+	else:
+		resultString = client.getKiValue("".join(("ecNumber*", re.split('\n',  l)[0]))).split('!')
+	print resultString
+	print l	 
 	for entry in resultString: #split each Brenda Km entry into a separate line
 		
 		all_fields = entry.split('#')
 		all_fields.remove('')
 		
-		if len(all_fields) == 8:
-			observedLigands.add(all_fields[7].split('*')[1]) #add ligandID to set of ligand IDs
+		if len(all_fields) != 8:
+			continue
+		observedLigands.add(all_fields[7].split('*')[1]) #add ligandID to set of ligand IDs
 		
 		entry_write = []
 		for field in all_fields:
 			entry_write.append(field.split('*')[1])
+		entry_write[4] = removeNonAscii(entry_write[4]) #some description include non ascii characters
+		
 		
 		ki_out.write("\t".join(entry_write) + "\n")
 	
@@ -70,11 +92,14 @@ ki_out.close()
 
 """ Determine the activity of activators values for all metabolites associated with an E.C. number """
 
-act_out = open("activator_file.tsv", "w")
-act_out.write("ecNumber\tactivatingCompound\torganism\tligandStructureId\tliterature" + "\n")
+act_out = open("".join((outfile, "_activators.tsv")), "w")
+act_out.write("ecNumber\tactivatingCompound\tcommentary\torganism\tligandStructureId\tliterature" + "\n")
 
 for l in open(ec_file):
-	resultString = client.getActivatingCompound("".join(("ecNumber*", re.split('\n',  l)[0], "#organism*", organism))).split('!')
+	if use_organism == 'TRUE':
+		resultString = client.getActivatingCompound("".join(("ecNumber*", re.split('\n',  l)[0], "#organism*", organism))).split('!')
+	else:
+		resultString = client.getActivatingCompound("".join(("ecNumber*", re.split('\n',  l)[0]))).split('!')
 	 
 	for entry in resultString: #split each Brenda Km entry into a separate line
 		
@@ -83,11 +108,13 @@ for l in open(ec_file):
 		all_fields = entry.split('#')
 		all_fields.remove('')
 		
-		if len(all_fields) == 6:
-			observedLigands.add(all_fields[5].split('*')[1]) #add ligandID to set of ligand IDs
+		if len(all_fields) != 6:
+			continue
+		observedLigands.add(all_fields[5].split('*')[1]) #add ligandID to set of ligand IDs
 		
 		for field in all_fields:
 			entry_write.append(field.split('*')[1])
+		entry_write[2] = removeNonAscii(entry_write[2]) #some description include non ascii characters
 		
 		act_out.write("\t".join(entry_write) + "\n")
 	
