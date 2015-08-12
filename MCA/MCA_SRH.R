3b0778458bc39786c4906309483ccba1d036057f
shackett <shackett@princeton.edu>
2015-08-12 11:06:37 -0400

    bu

--- MCA/MCA_SRH.R
+++ MCA/MCA_SRH.R
@@ -57,9 +57,9 @@ for(a_model in pathway_models){
                    PyK = "r_0962-rm-t_0276-inh-noncomp" # PyK
     )
     
-    modelRxnNames <- c(PFK = 'Isocitrate -| PFK',
-                       ALD = 'AMP -| ALD',
-                       PGM = '3PG -| PGM',
+    modelRxnNames <- c(PFK = 'PFK',
+                       ALD = 'ALD',
+                       PGM = 'PGM',
                        PyK = 'Citrate -| PyK') # optional renaming
     
     if(a_model == "Glycolysis with FBP feed-forward"){
@@ -122,12 +122,24 @@ for(a_model in pathway_models){
   # Add additional element to setup the summation rule
   MCAsummation <- data.frame(specie = "summation", unique(rxn_elasticities[,c('condition', 'reaction', 'markovSample')]), Elasticity = 1)
   rxn_elasticities <- rbind(MCAsummation, rxn_elasticities)
+  # change class of species and reaction to factors so that they are layed out canonically in the matrix
+  rxn_elasticities <- rxn_elasticities %>% mutate(specie = factor(specie, levels = c("summation", modelMet)), reaction = factor(reaction, levels = modelRxns))
   
   MCAmat <- acast(rxn_elasticities, specie ~ reaction ~ markovSample ~ condition, value.var = "Elasticity", fill = 0)
   # MCAmat[,,1,1]
   # For each condition & markov sample calculate control coefficients
-  controlCoef <- apply(MCAmat, c(3,4), function(x){solve(x)[,'summation']})
-  controlCoef_melt <- melt(controlCoef, varnames =  c("Reaction", "MarkovSample", "Condition"), value.name = "controlCoefficient")
+  
+  controlCoef <- apply(MCAmat, c(3,4), function(x){melt(solve(x), varnames = c("Reaction", "Specie"), value.name = "controlCoefficient", simplify = F)})
+  
+  # couldn't find a great solution to break down an array of lists
+  controlCoef_format <- expand.grid(markovSample = rownames(controlCoef), condition = colnames(controlCoef)) %>%
+    mutate(markovSample = as.numeric(markovSample), condition = as.character(condition))
+  
+  controlCoef <- lapply(1:nrow(controlCoef_format), function(x){
+    data.frame(markovSample = controlCoef_format$markovSample[x], condition = controlCoef_format$condition[x], 
+               controlCoef[controlCoef_format$markovSample[x],][[controlCoef_format$condition[x]]])
+  })
+  controlCoef_melt <- rbindlist(controlCoef)
   
   # Beautification - spruce up the naming
   
@@ -137,21 +149,43 @@ for(a_model in pathway_models){
   controlCoef_melt$rxnName <- factor(rxnLabels$RXN_RENAME[chmatch(as.character(controlCoef_melt$Reaction), rxnLabels$RXN_ID)], levels = rxnLabels$RXN_RENAME)
   
   # Conditions
-  controlCoef_melt$Condition = factor(controlCoef_melt$Condition, levels = paste0(rep(c("P", "C", "N", "L", "U"), each = 5), rep(c("0.05", "0.11", "0.16", "0.22", "0.30"), times = 5)))
-  controlCoef_melt$Limitation <- factor(substr(controlCoef_melt$Condition, 1, 1), levels = c("P", "C", "N", "L", "U"))
+  controlCoef_melt$condition = factor(controlCoef_melt$condition, levels = paste0(rep(c("P", "C", "N", "L", "U"), each = 5), rep(c("0.05", "0.11", "0.16", "0.22", "0.30"), times = 5)))
+  controlCoef_melt$limitation <- factor(substr(controlCoef_melt$condition, 1, 1), levels = c("P", "C", "N", "L", "U"))
   
+  controlCoef_melt <- controlCoef_melt %>% mutate(controlCoefficient = ifelse(Specie == "summation", controlCoefficient, -1*controlCoefficient))
+  
+  one_condition_control <- controlCoef_melt %>% filter(condition == "N0.05") %>% tbl_df()
+  one_condition_plot_bounds <- one_condition_control %>% group_by(rxnName, Specie) %>% summarize(LH = quantile(controlCoefficient, probs = c(0.25)) - diff(quantile(controlCoefficient, probs = c(0.25, 0.75))),
+                                                                                                 UH = quantile(controlCoefficient, probs = c(0.75)) + diff(quantile(controlCoefficient, probs = c(0.25, 0.75))))
+  one_condition_control <- one_condition_control %>% left_join(one_condition_plot_bounds)
+  one_condition_control <- one_condition_control %>% filter(controlCoefficient < UH & controlCoefficient > LH)
+  
+  
+  boxplot_theme <- theme(text = element_text(size = 20, face = "bold"), title = element_text(size = 25, face = "bold"), 
+                       panel.background = element_rect(fill = "gray92"), legend.position = "top", 
+                       axis.ticks = element_line(color = "black", size = 1),
+                       axis.text = element_text(color = "black", size = 20),
+                       panel.grid.minor = element_blank(), panel.grid.major.x = element_blank(), panel.grid.major.y = element_line(size = 1),
+                       axis.line = element_line(color = "black", size = 1), legend.title=element_blank()
+                       )
+  
+  ggplot(one_condition_control, aes(x = Specie, y = controlCoefficient, fill = rxnName, group = rxnName)) +
+    geom_boxplot(outlier.size = 0, alpha = 0.7) + facet_wrap(~ Specie, shrink = T, scale = "free", nrow = 1) +
+    boxplot_theme
+    
+  ggsave(file = paste0("MCA_plots/concentrationControl_", a_model, ".pdf"), width = nrow(rxnLabels) * 4, height = 6)
   
   boxplot_theme <- theme(text = element_text(size = 25, face = "bold"), title = element_text(size = 25, face = "bold"), panel.background = element_rect(fill = "mintcream"), axis.title.y = element_text(vjust = 0.35), 
                          legend.position = "top", panel.grid = element_blank(), axis.ticks.x = element_blank(), axis.ticks.y = element_line(size = 1, color = "chartreuse4"),
                          axis.text = element_text(color = "black", hjust = 1), axis.text.x = element_text(size = 20, angle = 90, hjust = 1), axis.line = element_line(size = 2, color = "chartreuse4"), axis.line.x = element_blank(), 
                          panel.margin = unit(1.5, "lines"), strip.background = element_rect(fill = "darkseagreen2"), strip.text = element_text(size = 25, vjust = 0.6, colour = "darkblue"))
   
-  ggplot(controlCoef_melt, aes(x = Condition, y = controlCoefficient, fill = Limitation)) + geom_hline(yintercept = -0.02, size = 4, color = "chartreuse4") +
+  ggplot(controlCoef_melt %>% filter(Specie == "summation"), aes(x = condition, y = controlCoefficient, fill = limitation)) + geom_hline(yintercept = -0.02, size = 4, color = "chartreuse4") +
     #geom_jitter(position = position_jitter(width = .4), size = 0.3, alpha = 0.5) +
     geom_boxplot(outlier.size = 0, alpha = 0.7) + facet_grid(rxnName ~ .) +
     boxplot_theme + scale_fill_brewer(palette = "Set2", guide = F) +
-    scale_x_discrete("") + scale_y_continuous("Pathway Control Coefficient", expand = c(0,0))
+    scale_x_discrete("") + scale_y_continuous("Pathway Control Coefficient", expand = c(0,0), limits = c(0,1))
   
-  ggsave(file = paste0("MCA_plots/", a_model, ".pdf"), width = 10, height = nrow(rxnLabels) * 4)
+  ggsave(file = paste0("MCA_plots/fluxControl_", a_model, ".pdf"), width = 10, height = nrow(rxnLabels) * 4)
   
 }
